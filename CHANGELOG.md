61174d91666ffd391f6f3e814bf70bb4da31e14c
SosnoviyBor <wormer007@gmail.com>
2025-09-10 17:21:21 +0300

    1.0.1 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # Dead Mer Tell No Tales (OpenMW)
 
+## 1.0.1
+
+### !! Dead actor list will reset on mod update !!
+
+### Fixes
+
+- Dead actor lists are now actually save-specific and not global
+
 ## 1.0
 
 Initial release
--- scripts/DeadMerTellNoTales/global.lua
+++ scripts/DeadMerTellNoTales/global.lua
@@ -1,8 +1,4 @@
-local storage = require("openmw.storage")
-
-require("scripts.DeadMerTellNoTales.cellBlacklist")
-
-local sectionDeadActors = storage.globalSection("deadActors")
+local world = require("openmw.world")
 
 local function disown(objects)
     for _, object in ipairs(objects) do
@@ -12,21 +8,16 @@ end
 
 local function onObjectActive(object)
     if object.owner.recordId ~= nil then
-        if sectionDeadActors:get(object.owner.recordId) then
-            -- check blacklist
-            if CellBlacklist[string.lower(object.cell.name)] then return end
-            if object.cell.isExterior then
-                local cellCoords = tostring(object.cell.gridX) .. "," .. tostring(object.cell.gridY)
-                if CellBlacklist[cellCoords] then return end
-            end
-
-            disown({object})
+        for _, player in ipairs(world.players) do
+            player:sendEvent("checkActorStatus", object)
         end
     end
 end
 
 local function recordDead(recordId)
-    sectionDeadActors:set(recordId, true)
+    for _, player in ipairs(world.players) do
+        player:sendEvent("setDeadActor", recordId)
+    end
 end
 
 return {
